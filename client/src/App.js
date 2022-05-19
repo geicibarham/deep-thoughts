75a95faa9ddf2576017b01dbbfe1fab768395607
geicibarham <geicianecosta43@gmail.com>
2022-05-19 14:49:37 -0400

    token now is saved to local storage and deleted when user clicks on logout

--- client/src/App.js
+++ client/src/App.js
@@ -9,15 +9,28 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+})
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+
 });
+
 function App() {
   return (
     <ApolloProvider client={client}>
